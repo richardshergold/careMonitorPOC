7bc57127f8abfd10d585d40b762f3daa9efe7c6d
Richard Shergold <richard@crossplatformsolutions.co.uk>
2017-03-01 09:07:00 +0000

    add zone and comment

--- src/pages/monitor/monitor.ts
+++ src/pages/monitor/monitor.ts
@@ -1,6 +1,7 @@
 // core stuff
 import { Component } from '@angular/core';
 import { NavController, Platform } from 'ionic-angular';
+import { NgZone } from "@angular/core";
 
 // providers
 import { BeaconProvider } from '../../providers/beacon-provider';
@@ -17,6 +18,7 @@ import { State } from '../../models/state-model';
 export class MonitorPage {
 
   state: State;
+  zone: any;
 
   constructor(
     public navCtrl: NavController,
@@ -24,6 +26,11 @@ export class MonitorPage {
     public beaconProvider: BeaconProvider,
     public firebaseProvider: FirebaseProvider,
   ) {
+
+    // a zone required for UI update - I need to learn a bit more about
+    // zones (execution contexts) as I wasn't getting a UI change here but
+    // with a zone in place it updatesa
+    this.zone = new NgZone({ enableLongStackTrace: false });
 
     // tried to use some best practice (?) MVC architecture here
     // keeping the component very thing with logic in the service 
@@ -38,7 +45,11 @@ export class MonitorPage {
     // I could have used events here instead (which is what I've tended to do in the past)
     // but this was a good opportunity to try my own observable on a class and subscribe to it
     this.state.stateWatch.subscribe(status => {
-      this.firebaseProvider.logEvent(status);
+      // running this inside my zone but need to do further tests to see
+      // whether or not the zone is needed here as all I'm doing inside is calling the Firebase update ** TO-DO **
+      this.zone.run(() => {
+        this.firebaseProvider.logEvent(status);
+      });
     });
   }
 
