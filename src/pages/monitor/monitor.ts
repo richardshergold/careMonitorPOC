40819ed3af47a6574a54c03a595bd8f4ac1e9587
Richard Shergold <richard@crossplatformsolutions.co.uk>
2017-03-01 07:31:11 +0000

    update comments

--- src/pages/monitor/monitor.ts
+++ src/pages/monitor/monitor.ts
@@ -17,7 +17,6 @@ import { State } from '../../models/state-model';
 export class MonitorPage {
 
   state: State;
-  zone: any
 
   constructor(
     public navCtrl: NavController,
@@ -26,19 +25,25 @@ export class MonitorPage {
     public firebaseProvider: FirebaseProvider,
   ) {
 
+    // tried to use some best practice (?) MVC architecture here
+    // keeping the component very thing with logic in the service 
+    // and using an object/model for the state
+
     // create an instance of the state class
     this.state = new State(false);
 
     // subscribe to the observable on the state which returns
     // a result every time the status changes, when it does call the logEvent
     // method in the firebase service to log the event at the back-end
+    // I could have used events here instead (which is what I've tended to do in the past)
+    // but this was a good opportunity to try my own observable on a class and subscribe to it
     this.state.stateWatch.subscribe(status => {
       this.firebaseProvider.logEvent(status);
     });
   }
 
   ionViewDidLoad() {
-    // wait for platform ready as we are needing a plugin
+    // wait for platform ready as we are needing to use the beacons plugin
     this.platform.ready().then(() => {
       // initialise the beacon service which will commence scanning
       // we pass accross the state object as a paramater
